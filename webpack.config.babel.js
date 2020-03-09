3439063305976e5f0536a2fe626c60ff4c6c1a7b
faif <faif@pikselan.com>
2020-03-09 07:24:07 +0700

    init node

---
+++ webpack.config.babel.js
@@ -0,0 +1,29 @@
+import path from 'path';
+
+export default {
+  context: path.resolve(__dirname),
+  entry: {
+    app: './src'
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js','.jsx']
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?/,
+      loader: 'babel-loader',
+      include: [path.join(__dirname, 'src')]
+    },{
+      test: /\.css/,
+      loader: 'style-loader!css-loader'
+    }]
+  },
+  devServer: {
+    inline: true
+  }
+}
